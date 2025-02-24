1d62ae3c9090449834b4233b6bba2d59ea13b8fe
Lance Wright <lancewrightjr2@gmail.com>
2025-02-24 11:31:10 -0500

    Just pushing a change to force the approval to go away. This isn't ready

--- Magic.IndexedDb/Models/MagicContractResolver.cs
+++ Magic.IndexedDb/Models/MagicContractResolver.cs
@@ -1,13 +1,8 @@
 ﻿using Magic.IndexedDb.SchemaAnnotations;
-using System;
 using System.Collections.Concurrent;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
-using System.Text;
 using System.Text.Json.Serialization;
 using System.Text.Json;
-using System.Threading.Tasks;
 
 namespace Magic.IndexedDb.Models
 {
